d561f79705590c35e10fb7c94d12f1aa7cb12c62
danreyrey <danielreynders87@gmail.com>
2026-09-25 14:33:44 +0200

    Refactor Message method with alternative implementations
    Commented out alternative implementations for message extraction and log level parsing.

--- solutions/csharp/log-levels/2/LogLevels.cs
+++ solutions/csharp/log-levels/2/LogLevels.cs
@@ -6,15 +6,23 @@ static class LogLine
     {
         char[] charArray = {'\n', '\r',  '\t', '\\', '\"', '\'', '\0', '\e', ' '};
         return logLine.Substring(logLine.IndexOf(':') + 1).Trim(charArray);
-    }
 
+        // logLine = logLine[(logLine.IndexOf(':') + 1)..].Trim().Trim('!');
+		// logLineCopy = logLineCopy[1..(logLineCopy.IndexOf(']'))].ToLower();
+		
+		// logLine = logLine.Split(':')[1].Trim().Trim('!');
+		// logLineCopy = logLineCopy.Split(']')[0].Trim('[').ToLower();
+		
+		// char[] arr = logLine.ToCharArray();
+		// Array.Reverse(arr);
+		// string rev = new string(arr);
+    }
 
     public static string LogLevel(string logLine)
     {
         char[] charArray = {'[', ']'};
         return logLine.Substring(0, logLine.IndexOf(':')).Trim(charArray).ToLower();
     }
-
 
     public static string Reformat(string logLine)
         => $"{Message(logLine)} ({LogLevel(logLine)})";
